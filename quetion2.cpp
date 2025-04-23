7839a6b927709b5014b7eaee4c140f37d50b625c
Priteshsolanki12 <solankipritesh226@gmail.com>
2025-04-23 18:37:29 +0530

    add a two  quetipon

---
+++ quetion2.cpp
@@ -0,0 +1,16 @@
+// add all number 1 to 10 in c++ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+
+    for(int i = 1; i <= 10; i++) {
+        sum += i;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
